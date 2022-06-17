7eee8779fc286a214919e42b0b4ca1f2a70623e8
Ga-Rasquinho <gabriel_rasquinho@outlook.com>
2022-06-17 19:28:51 -0300

    Adicionando busca binaria

--- vetores.c
+++ vetores.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 
 int main(){
+    // Variaveis para busca binaria
+    int inicio;
+    int meio;
+    int final;
+    int dadoEncontrado;
+    int busca;    
+    // Vetores   
     int vet1[5] = {5,6,7,8,9};
     int vet2[5] = {0,1,2,3,4};
     int vet3[10];
@@ -19,13 +26,34 @@ int main(){
     // Bubble sort para ordernar os vetores
     for(i = 0; i < 10; i++){
         for(x = 0; x < 9; x++){
-            if(vet3[x] < vet3[x + 1]){
+            if(vet3[x] > vet3[x + 1]){
                 aux = vet3[x];
                 vet3[x] = vet3[x + 1];
                 vet3[x + 1] = aux;
             }
         }
     }
-    
+    // Mostrar valores na tela
+    for(i = 0; i < 10; i++){
+        printf("[%d]\t", vet3[i]);
+    }
+    printf("\nDigite o valor que deseja buscar: ");
+    fflush(stdin); scanf("%d", &busca);
+    // Inicializacao dos valores
+    inicio = 0;
+    final = 9;
+    dadoEncontrado = 0;
+    // Busca binaria
+    while(inicio <= final && dadoEncontrado == 0){
+        meio = (inicio + final) / 2;
+        if(vet3[meio] == busca){
+            printf("Dado encontrado na posicao: #%d", meio);
+            dadoEncontrado++;
+        }if(vet3[meio] > busca){
+            final = meio - 1;
+        }else{
+            inicio = meio + 1;
+        }
+    }
     return 0;
 }
