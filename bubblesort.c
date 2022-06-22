4efed9d7b3bfb599faf7467c074e01886f0dbd08
Ga-Rasquinho <gabriel_rasquinho@outlook.com>
2022-06-22 11:05:23 -0300

    Inserindo a constante e números aleatórios

--- bubblesort.c
+++ bubblesort.c
@@ -1,58 +1,32 @@
+/* Bibliotecas */
 #include <stdio.h>
 #include <stdlib.h>
-
-int main()
-{
-    int opcao;
-    int vet[5] = {5, 4, 3, 2, 1};
-    int i, x, z, aux;
-    for (i = 0; i < 5; i++)
-    {
-        for (x = 0; x < 4; x++)
-        {
-            if (vet[x] > vet[x + 1])
-            {
-                aux = vet[x];
-                vet[x] = vet[x + 1];
-                vet[x + 1] = aux;
-            }
-        }
-    }
-    for (z = 0; z < 5; z++)
-    {
-        printf("%d\t", vet[z]);
-    }
-    int inicial = 0;
-    int meio;
-    int final = 4;
-    int dadoEncontrado = 0;
-    printf("\nBuscar valor: ");
-    fflush(stdin);
-    scanf("%i", &opcao);
-
-    while (inicial <= final && dadoEncontrado == 0)
-    {
-        meio = (inicial + final) / 2;
-        if (vet[meio] == opcao)
-        {
-            dadoEncontrado++;
-        }
-        if (vet[meio] > opcao)
-        {
-            final = --meio;
-        }
-        else
-        {
-            inicial = ++meio;
-        }
-    }
-    if (dadoEncontrado > 0)
-    {
-        printf("\nDado encontrado na posicao: %i", meio);
-    }
-    else
-    {
-        printf("Dado nao encontrado");
-    }
-    return 0;
+#include <conio.h> // Funciona apenas no windows
+#include <time.h>
+#define TAM 10 /* Constante */
+/* Variáveis globais */
+int i, contador, x, vet[TAM], guardar, contPrintar;
+/* Corpo do programa */
+int main(){
+	/* Gerando númeroes aleatórios dentro do vetor */
+	srand(time(NULL));
+	for(i = 0; i < TAM; i++){
+		vet[i] = rand () % 100;
+	}
+	/* Ordenação */
+	for(contador = 0; contador < TAM; contador++){
+		for(x = 0; x < TAM - 1; x++){
+			if(vet[x] > vet[x + 1]){
+				guardar = vet[x];
+				vet[x] = vet[x + 1];
+				vet[x + 1] = guardar;
+			}
+		}
+	}
+	/* Mostrar os números dos vetores na tela já ordenados*/
+	for(contPrintar = 0; contPrintar < TAM; contPrintar++){
+		printf("%d\t", vet[contPrintar]);
+	}
+	getch();
+	return 0;
 }
